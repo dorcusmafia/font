c4cf7d18e641a222f1c6b6dc6b1c9de61b7fdd06
constructit <constructit@users.noreply.github.com>
2017-02-23 21:06:54 +0100

    Sync scripts.

---
+++ init.js
@@ -0,0 +1,44 @@
+﻿require.config({ 
+    waitSeconds: 200,
+    baseUrl: 'scripts/',
+    paths: {
+        'bootstrap': 'lib/bootstrap',
+        'bootstrap-toolkit': 'lib/bootstrap-toolkit',
+        'hypher': 'lib/jquery.hypher',
+        'hypher-sv': 'lib/sv',
+        'jquery': 'lib/jquery',
+        'knockout': 'lib/knockout-latest',
+        'ko.mapping': 'lib/knockout.mapping',
+        'moment': 'lib/moment-with-locales',
+        'scrollto': 'lib/jquery.scrollTo',
+        'localscroll': 'lib/jquery.localScroll',
+        'select2': 'lib/select2',
+        'sticky-kit': 'lib/sticky-kit',
+        'tether': 'lib/tether',
+        'text': 'lib/text',
+    },
+
+    shim: {
+        'bootstrap': {
+            deps: ['jquery', 'tether']
+        },
+        'bootstrap-toolkit': {
+            deps: ['jquery']
+        },
+        'hypher-sv': {
+            deps: ['jquery', 'hypher']
+        },
+        'ko.mapping': {
+            deps: ['knockout']
+        },
+        'scrollto': {
+            deps: ['jquery']
+        },
+        'localscroll': {
+            deps: ['jquery', 'scrollto']
+        },
+        'sticky-kit': {
+            deps: ['jquery']
+        },
+    }
+});
